aa23197ce4a340f4a00d142e3cc5d8741484d23e
Tiago <tiago.ary10@gmail.com>
2023-09-27 20:04:57 -0300

    ajustes

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,9 +1,42 @@
 from pathlib import Path
-
+from PIL import Image
 from instagrapi import Client
 from instagrapi.types import StoryMention, UserShort
-from utils import read_credentials, save_posted_story, convert_heic_to_jpeg,\
-    verify_if_story_is_already_posted
+
+def verify_if_story_is_already_posted(story):
+    posted_stories = read_posted_stories()
+    if str(story.pk) in posted_stories:
+        print(f"Story {story} já foi postado.")
+        return True
+    return False
+
+
+def reset_posted_stories():
+    with open('posted_stories.txt', 'w') as f:
+        f.write('')
+
+
+def read_credentials():
+    with open('credentials.txt', 'r') as f:
+        return f.read().splitlines()
+
+
+def convert_heic_to_jpeg(heic_path, jpeg_path):
+    image = Image.open(heic_path)
+    image.convert('RGB').save(jpeg_path, 'JPEG')
+
+
+def read_posted_stories():
+    try:
+        with open('posted_stories.txt', 'r') as f:
+            return set(f.read().splitlines())
+    except FileNotFoundError:
+        return set()
+
+
+def save_posted_story(story_id):
+    with open('posted_stories.txt', 'a') as f:
+        f.write(f"{story_id}\n")
 
 
 class Instagram_bot:
@@ -16,12 +49,12 @@ class Instagram_bot:
     def login(self, username, password):
         self.client.login(username, password)
 
-    def get_following_accounts(self):
-        user_id = self.client.user_info_by_username(self.username).pk
+    def get_following_accounts(self, username=None):
+        user_id = self.client.user_info_by_username(username).pk
         return self.client.user_following(user_id)
 
-    def get_followers_accounts(self):
-        user_id = self.client.user_info_by_username(self.username).pk
+    def get_followers_accounts(self, username=None):
+        user_id = self.client.user_info_by_username(username).pk
         return self.client.user_followers(user_id)
 
     def get_followers_stories_with_self_mentions(self, user_followers):
