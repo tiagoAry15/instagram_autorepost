55cf9128419f339c6957e92ca91a2e8755c8b97e
Parjasnelly A <63004038+parjasnelly@users.noreply.github.com>
2023-11-09 16:46:37 -0300

    Salva a sessão para não ficar re-logando e adiciona suporte ao 2FA

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from PIL import Image
 from instagrapi import Client
+from instagrapi.exceptions import LoginRequired
 from instagrapi.types import StoryMention, UserShort
 
 
@@ -20,6 +21,11 @@ def reset_posted_stories():
 def read_credentials():
     with open('credentials.txt', 'r') as f:
         return f.read().splitlines()
+
+
+def read_verification_code():
+    with open('2FA_code.txt', 'r') as f:
+        return f.read()
 
 
 def convert_heic_to_jpeg(heic_path, jpeg_path):
@@ -42,13 +48,58 @@ def save_posted_story(story_id):
 
 class Instagram_bot:
     def __init__(self):
-        self.client = Client()
         username, password = read_credentials()
+        verification_code = read_verification_code()
         self.username = username
-        self.login(username, password)
+        self.login_user(username, password, verification_code)
+        self.client.dump_settings("session.json")
 
-    def login(self, username, password):
-        self.client.login(username, password)
+    def login_user(self, username, password, verification_code):
+        """
+        Attempts to login to Instagram using either the provided session information
+        or the provided username and password.
+        """
+
+        self.client = Client()
+        self.client.delay_range = [1, 3]
+        session = self.client.load_settings("session.json")
+
+        login_via_session = False
+        login_via_pw = False
+
+        if session:
+            try:
+                self.client.set_settings(session)
+                # resume session
+                self.client.login(username, password)
+
+                # check if session is valid
+                try:
+                    self.client.get_timeline_feed()
+                except LoginRequired:
+                    print("Session is invalid, need to login via username and password")
+
+                    old_session = self.client.get_settings()
+
+                    # use the same device uuids across logins
+                    self.client.set_settings({})
+                    self.client.set_uuids(old_session["uuids"])
+
+                    self.client.login(username, password)
+                login_via_session = True
+            except Exception as e:
+                print("Couldn't login user using session information: %s" % e)
+
+        if not login_via_session:
+            try:
+                print("Attempting to login via username, password and 2FA. username: %s" % username)
+                if self.client.login(username, password, verification_code=verification_code):
+                    login_via_pw = True
+            except Exception as e:
+                print("Couldn't login user using username, password and 2FA: %s" % e)
+
+        if not login_via_pw and not login_via_session:
+            raise Exception("Couldn't login user with either password or session")
 
     def get_following_accounts(self, username=None):
         user_id = self.client.user_info_by_username(username).pk
